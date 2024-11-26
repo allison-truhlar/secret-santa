ed385059c6785488026c609121f00a900355bbfd
allison-truhlar <truhlara@janelia.hhmi.org>
2024-11-26 16:58:05 -0500

    fix: change the group model schema to align with form input

--- server/models/groupModel.js
+++ server/models/groupModel.js
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const memberSchema = new Schema({ name: String });
-
 export const groupSchema = new Schema(
   {
     name: { type: String, required: true },
-    members: [{ type: memberSchema, required: true }],
+    members: [String],
     matches: [
       {
         year: Number,
@@ -22,4 +20,6 @@ export const groupSchema = new Schema(
   { timestamps: true }
 );
 
-export const Group = mongoose.model("Group", groupSchemaSchema);
+const Group = mongoose.model("Group", groupSchema);
+
+export default Group;

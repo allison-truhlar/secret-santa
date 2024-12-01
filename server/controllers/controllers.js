b44f30da05993f1fe24ed3f64259fa4edd2c7480
allison-truhlar <amtruhlar@gmail.com>
2024-12-01 16:06:54 -0500

    feat: adds matches to the group create controller

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,10 +3,28 @@ import Group from "../models/groupModel.js";
 export const createGroup = async (req, res) => {
   console.log("from server:", req.body);
   const { name, members } = req.body;
+  let receivers = []
+
+  const assignments = members.map((member, index) => {
+    let randIndex = index;
+    let receiver = null;
+    while (randIndex == index || receivers.includes(receiver)){
+      randIndex = Math.floor(Math.random() * members.length)
+      receiver = members[randIndex]
+    }
+    receivers.push(receiver)
+    return {giver: member, receiver: receiver}
+
+  })
+
+  const year = new Date().getFullYear()
+  const matches = [{year, assignments}]
+
   try {
     const group = await Group.create({
       name,
       members,
+      matches
     });
     res.status(200).json({ group, msg: "Success! Group added!" });
   } catch (error) {
